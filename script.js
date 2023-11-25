101b6d4f78e3d77ac295d6174be788b4fabb2d58
Awais Raees Ansari <80111573+AnsariAwaisRaees@users.noreply.github.com>
2023-11-25 19:02:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function isSameType(value1, value2) {
   //your js code here
-	if (isNaN(value1) && isNaN(value2)) {
+	if ((isNaN(value1) && isNaN(value2)) || (typeof value1 !== 'number' && typeof value2 !== 'number')) {
         return true;
-    } else {
-        return typeof value1 === typeof value2;
     }
+	
+    return typeof value1 === typeof value2;
 }
 
 // do not change the code below.
